5e66b8b976f2199944c65dcc486caa0a3a4ff007
Jayvardhan99 <25120cn058@glbitm.ac.in>
2026-02-24 22:24:18 +0530

    Create jay 2.c

---
+++ jay 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter number");
+    scanf("%d",&a );
+    if (a % 2 == 0)
+    {
+        printf("even number");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+    return 0;
+}
